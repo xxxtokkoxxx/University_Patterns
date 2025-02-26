90605d3de5b68be90f10d8df029d4819caaa9205
Bohdan Korniichuk <b.korniichuk@8allocate.com>
2025-02-26 11:07:39 +0200

    fix: typos

--- Patterns/obj/Debug/net8.0/Patterns.AssemblyInfo.cs
+++ Patterns/obj/Debug/net8.0/Patterns.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538fe9c330ff66f481742f65f55fff13755e1fbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc3b7e88a3b302f11e7ab60d4d46d213ce8a927")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
